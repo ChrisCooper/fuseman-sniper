d5b8f71538f9a47fa598b701870095ed078b2717
Chris Cooper <chris@quixotic-chris.com>
2016-07-05 22:03:34 -0400

    Add proper scope view

---
+++ Assets/Scripts/Lifetime.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lifetime : MonoBehaviour {
+
+    public float secondsToLive = 3;
+	void Awake() {
+        Destroy(gameObject, 1);
+    }
+}
